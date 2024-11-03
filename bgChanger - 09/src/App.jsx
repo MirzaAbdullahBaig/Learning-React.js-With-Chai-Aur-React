30bed151c6e16eec9ae46ce29b96c8a8291183d5
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-03 11:23:12 +0500

    bgChanger - 09 || Project 01

--- bgChanger - 09/src/App.jsx
+++ bgChanger - 09/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./Components/Button";
+import Btndata from "./Data/Btndata";
 
 function App() {
   const [color, setColor] = useState("olive");
@@ -8,17 +9,16 @@ function App() {
     <div className="w-full h-screen" style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-5 py-5 rounded-3xl">
-          <Button setColor={setColor} colorEnter={"#dc2626"} text={"Red"} bgcolor={"bg-red-600 text-white"} />
-          <Button setColor={setColor} colorEnter={"#16a34a"} text={"Green"} bgcolor={"bg-green-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#2563eb"} text={"Blue"} bgcolor={"bg-blue-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#9333ea"} text={"Purple"} bgcolor={"bg-purple-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#65a30d"} text={"Lime"} bgcolor={"bg-lime-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#4b5563"} text={"Gray"} bgcolor={"bg-gray-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#ca8a04"} text={"Yellow"} bgcolor={"bg-yellow-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#db2777"} text={"Pink"} bgcolor={"bg-pink-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"#475569"} text={"Slate"} bgcolor={"bg-slate-600 text-white"}/>
-          <Button setColor={setColor} colorEnter={"black"} text={"Black"} bgcolor={"bg-black text-white"}/>
-          <Button setColor={setColor} colorEnter={"#cbd5e1"} text={"White"} bgcolor={"bg-slate-300 text-blak"}/>
+          {Btndata.map(({colorEnter, text, bgcolor}) => {
+            return (
+              <Button
+                setColor={setColor}
+                colorEnter={colorEnter}
+                text={text}
+                bgcolor={bgcolor}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
---
+++ bgChanger - 09/src/Data/Btndata.jsx
@@ -0,0 +1,59 @@
+const Btndata = [
+  {
+    colorEnter: "#dc2626",
+    text: "Red",
+    bgcolor: "bg-red-600 text-white",
+  },
+  {
+    colorEnter: "#16a34a",
+    text: "Green",
+    bgcolor: "bg-green-600 text-white",
+  },
+  {
+    colorEnter: "#2563eb",
+    text: "Blue",
+    bgcolor: "bg-blue-600 text-white",
+  },
+  {
+    colorEnter: "#9333ea",
+    text: "Purple",
+    bgcolor: "bg-purple-600 text-white",
+  },
+  {
+    colorEnter: "#65a30d",
+    text: "Lime",
+    bgcolor: "bg-lime-600 text-white",
+  },
+  {
+    colorEnter: "#4b5563",
+    text: "Gray",
+    bgcolor: "bg-gray-600 text-white",
+  },
+  {
+    colorEnter: "#ca8a04",
+    text: "Yellow",
+    bgcolor: "bg-yellow-600 text-white",
+  },
+  {
+    colorEnter: "#db2777",
+    text: "Pink",
+    bgcolor: "bg-pink-600 text-white",
+  },
+  {
+    colorEnter: "#475569",
+    text: "Slate",
+    bgcolor: "bg-slate-600 text-white",
+  },
+  {
+    colorEnter: "black",
+    text: "Black",
+    bgcolor: "bg-black text-white",
+  },
+  {
+    colorEnter: "#cbd5e1",
+    text: "White",
+    bgcolor: "bg-slate-300 text-black",
+  },
+];
+
+export default Btndata;
