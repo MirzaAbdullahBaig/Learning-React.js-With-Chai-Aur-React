cf8210df35b50937f80da553e59e090b0ce0a391
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-21 09:30:49 +0500

    some changes

--- Currency Converter - 11/src/hooks/useCurrencyInfo.js
+++ Currency Converter - 11/src/hooks/useCurrencyInfo.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-
+import axios from "axios";
 
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({})
+  const API_KEY = `https://v6.exchangerate-api.com/v6/41740f242f8dfdd871e297aa/latest/${currency}`
   
   useEffect(() => {
-    fetch(`https://v6.exchangerate-api.com/v6/41740f242f8dfdd871e297aa/latest/${currency}`)
+    fetch(API_KEY)
     .then((res) => res.json())
     .then((res) => setData(res[currency]))
     console.log(data);
