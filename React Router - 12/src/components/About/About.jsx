807f433866b2e66dcc4fad78888472a7e108ead1
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-27 11:44:48 +0500

    React Router - 12

--- React Router - 12/src/components/About/About.jsx
+++ React Router - 12/src/components/About/About.jsx
@@ -7,7 +7,8 @@ export default function About() {
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-5/12">
             <img
-              src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+            className="rounded-lg"
+              src="https://images.pexels.com/photos/248159/pexels-photo-248159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="image"
             />
           </div>
