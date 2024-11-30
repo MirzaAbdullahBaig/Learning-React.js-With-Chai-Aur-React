738f11fae08f4d823f43d130a8bf43f9918f4a67
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-30 07:55:25 +0500

    Theme Changer - 13.2

--- Context API - 13/Theme Changer - 13.2/src/App.jsx
+++ Context API - 13/Theme Changer - 13.2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "./context/theme";
 import Card from "./components/Card";
 import ThemeBtn from "./components/ThemeBtn";
