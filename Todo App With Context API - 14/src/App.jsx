8b25bbdf4a786ec451cd3e30e70005abf10a2213
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-30 09:53:42 +0500

    Todo App With Context API - 14

---
+++ Todo App With Context API - 14/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl text-center text-blue-600 hover:bg-blue-600 hover:text-white p-4 mt-4 font-extrabold duration-700 ease-out">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
