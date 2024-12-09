b4e92f3a8f5fb4f5476cb131a8ecdb2c5404e0b9
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-12-09 11:38:24 +0500

    Todo With Redux Toolkit - 15

--- Todo With Redux Toolkit - 15/src/features/todo/todoSlice.js
+++ Todo With Redux Toolkit - 15/src/features/todo/todoSlice.js
@@ -9,6 +9,9 @@ export const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state, action) => {
+      if (action.payload.trim() === "") {
+        return; // Exit without doing anything if the todo is empty
+      }
       const todo = {
         id: nanoid(),
         text: action.payload
@@ -18,9 +21,12 @@ export const todoSlice = createSlice({
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todos) => todos.id !== action.payload)
     },
+    editTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => todo.id === action.payload.id ? { ...todo, text: action.payload.text } : todo)
+    },
   }
 })
 
-export const { addTodo, removeTodo } = todoSlice.actions
+export const { addTodo, removeTodo, editTodo } = todoSlice.actions
 
 export default todoSlice.reducer
